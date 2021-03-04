dd1c7d51b88e747b57997e7c4e747cac8476ee6c
Ferdian Julianto <juliantoferdian24@gmail.com>
2021-03-04 17:03:07 +0700

    modul 1

---
+++ Job.java
@@ -0,0 +1,48 @@
+public class Job
+{
+    // instance variables - replace the example below with your own
+    private int id;
+    private String name;
+    private int fee;
+    private String category;
+    
+    public Job()
+    {
+        // initialise instance variables
+    }
+
+    public static int getId()
+    {
+        return 0;
+    }
+    public static String getName()
+    {
+        return "oop";
+    }
+    public static int getFee()
+    {
+        return 0;
+    }
+    public static String getCategory()
+    {
+        return "oop";
+    }
+    public static void setId(int id)
+    {
+       
+    }
+    public static void setName(String name)
+    {
+       
+    }
+    public static void setFee(int fee)
+    {
+       
+    }
+    public static void setCategory(String category)
+    {
+        
+    }
+    
+    
+}
---
+++ Jwork.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Jwork here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Jwork
+{
+    
+    public Jwork()
+    {
+       
+    }
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
