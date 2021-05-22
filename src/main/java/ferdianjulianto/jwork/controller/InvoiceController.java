de65fc02ca26800e500d21ca3333bf9e430cbcca
Ferdian Julianto <juliantoferdian24@gmail.com>
2021-05-22 09:24:29 +0700

    MODUL 8 - POST TEST - Teks Bebas

---
+++ src/main/java/ferdianjulianto/jwork/controller/InvoiceController.java
@@ -0,0 +1,81 @@
+package ferdianjulianto.jwork.controller;
+
+import ferdianjulianto.jwork.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RequestMapping("/invoice")
+@RestController
+public class InvoiceController {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ArrayList<Invoice> getAllInvoice() {
+        return (DatabaseInvoice.getInvoiceDatabase();
+    }
+
+    @RequestMapping("/{id}")
+    public Invoice getInvoiceById(@PathVariable int id) {
+        Invoice invoice = null;
+        try {
+            invoice = DatabaseInvoice.getInvoiceById(id);
+        } catch (InvoiceNotFoundException e) {
+            e.getMessage();
+            return null;
+        }
+        return invoice;
+    }
+
+    @RequestMapping("/Jobseeker/{JobseekerId}")
+    public ArrayList<Invoice> getInvoiceByJobseeker(@PathVariable int jobseekerid) {
+        ArrayList<Invoice> invoices = null;
+        invoices = DatabaseInvoice.getInvoiceByJobseeker(jobseekerid);
+
+        return invoices;
+    }
+
+    //no5
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public boolean removeInvoice(@PathVariable int id) {
+        try {
+            DatabaseInvoice.removeInvoice(id);
+            return true;
+        } catch (InvoiceNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @RequestMapping(value = "invoiceStatus/{id}", method = RequestMethod.PUT)
+    public Invoice changeInvoiceStatus(@PathVariable int id,
+                                       @RequestParam(value = "status") InvoiceStatus status){
+        Invoice invoice = null;
+        try {
+            invoice = DatabaseInvoice.getInvoiceById(id);
+            invoice.setInvoiceStatus(status);
+            return invoice;
+        } catch (InvoiceNotFoundException e) {
+            e.getMessage();
+            return null;
+        }
+
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Invoice addBankPayment(@RequestParam(value="foodIdList") ArrayList<Integer> foodIdList,
+                      @RequestParam(value="Jobseekeri") int customerId,
+                      @RequestParam(value="adminFee") int adminFee)
+    {
+        Job job = null;
+        try {
+            job = new Job(DatabaseJob.getLastId()+1, name, DatabaseRecruiter.getRecruiterById(recruiterId), fee, JobCategory.valueOf(category));
+        } catch (RecruiterNotFoundException e) {
+            e.getMessage();
+        }
+        boolean status = DatabaseJob.addJob(job);
+        if(status == true){
+            return job  ;
+        } else {
+            return null;
+        }
+    }
+}
